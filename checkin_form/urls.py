b070acf5f8f014fd23d8f4a6b6eaabef53346171
unknown <somanshS14@gmail.com>
2021-04-29 02:00:28 +0530

    Update working

--- checkin_form/urls.py
+++ checkin_form/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import HomeView, CheckInView,InitialView
+from .views import *
 
 urlpatterns = [
     path('',InitialView.as_view()),
     path('checkin',CheckInView.as_view()),
     path('home',HomeView.as_view()),
+    path('update/<int:id>',UpdateView.as_view()),
+    path('delete/<int:id>',DeleteView.as_view()),
 ]
--- checkin_form/views.py
+++ checkin_form/views.py
@@ -27,14 +27,62 @@ class CheckInView(View):
             form.save()
             return redirect('/home')
 
-# class UpdateView(View):
-#     def get(self, request):
-#         obj = CheckInModel.objects.all()  
-#         return render(request,"update.html",{'dataObjects':obj})  
+class UpdateView(View):
+    def get(self, request,id):
+        obj = CheckInModel.objects.get(id=id)  
+        return render(request,"update.html",{'data':obj})  
 
-# class Delete(View):
-#     def get(self,request,id):
-#         obj = CheckInModel.objects.get(id=id)  
-#         obj.delete()  
-#         return redirect("/home")      
+    def post(self,request,id):
+        obj = CheckInModel.objects.get(id=id)  
+        for i in request.POST:
+            if i == 'csrfmiddlewaretoken':
+                continue
+            if i == 'first_name':
+                if obj.first_name != request.POST['first_name']:
+                    obj.first_name = request.POST['first_name']
+            if i == 'last_name':
+                if obj.last_name != request.POST['last_name']:
+                    obj.last_name = request.POST['last_name']
+            if i == 'email':
+                if obj.email != request.POST['email']:
+                    obj.email = request.POST['email']
+            if i == 'age':
+                if obj.age != request.POST['age']:
+                    obj.age = request.POST['age']
+            if i == 'gender':
+                if obj.gender != request.POST['gender']:
+                    obj.gender = request.POST['gender']
+            if i == 'address':
+                if obj.address != request.POST['address']:
+                    obj.address = request.POST['address']
+            if i == 'checkin':
+                if obj.checkin != request.POST['checkin'] and request.POST['checkin'] != '':
+                    obj.checkin = request.POST['checkin']
+            if i == 'checkout':
+                if obj.checkout != request.POST['checkout'] and request.POST['checkout'] != '':
+                    obj.checkout = request.POST['checkout']
+            if i == 'payment_method':
+                if obj.payment_method != request.POST['payment_method']:
+                    obj.payment_method = request.POST['payment_method']
+            if i == 'room_allocated':
+                if obj.room_allocated != request.POST['room_allocated']:
+                    obj.room_allocated = request.POST['room_allocated']
+            if i == 'additional_choices':
+                if obj.additional_choices != request.POST['additional_choices']:
+                    obj.additional_choices = request.POST['additional_choices']
+            if i == 'file_field':
+                if request.POST['file_field'] != '':
+                    obj.file_field = request.FILES['file_field']
+        for i in request.FILES:
+            if i == 'file_field':
+                if request.FILES['file_field'] != '':
+                    obj.file_field = request.FILES['file_field']
+        obj.save()
+        return redirect("/home")  
+
+class DeleteView(View):
+    def get(self,request,id):
+        obj = CheckInModel.objects.get(id=id)  
+        obj.delete()  
+        return redirect("/home")      
 
